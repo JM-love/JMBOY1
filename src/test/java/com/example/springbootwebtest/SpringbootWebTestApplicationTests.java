0605bacc3206eaec3d9e8892818f17bfcc2926a5
“JM-lovegit config --global user.name “JM-love <2624733498@qq.com;git config --global --listgit config --global --list;git config --global user.email 2624733498@qq.com>
2022-10-31 22:23:20 +0800

    test

---
+++ src/test/java/com/example/springbootwebtest/SpringbootWebTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
